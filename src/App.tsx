0493bb5de8af351446e909ebfded5d888e66a231
rohit <rohitoriginal37@gmail.com>
2024-01-15 06:59:52 +0530

    added new button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Input from "./components/Input";
+import Button from "./components/Button";
 
 function App() {
   return <main>
     <Input label="Simple" id="simple" type="text"/>
+    <Button el='button'>Whatever</Button>
+    <Button el='anchor'>A link i presume</Button>
   </main>
 }
 
